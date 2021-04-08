1d4ab8cbf309eacb67d5183f7dc721c5aa15937e
rutger <36332815+rutger-t@users.noreply.github.com>
2021-04-09 07:37:22 +0900

    setup devise with json

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+
+  def render_jsonapi_response(resource)
+    if resource.errors.empty?
+      render json: UserSerializer.new(resource).serializable_hash.to_json
+    else
+      render json: { error: resource.errors }, status: 400
+    end
+  end
+
 end
