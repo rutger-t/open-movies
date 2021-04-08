eac7ed464f0d7de255fa47ba0f75db9a1bb44652
rutger <36332815+rutger-t@users.noreply.github.com>
2021-04-09 07:36:10 +0900

    add user serializer

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
         :jwt_authenticatable, jwt_revocation_strategy: JwtDenylist
+
+  validates :email, uniqueness: true
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,8 +1,4 @@
 class UserSerializer
   include JSONAPI::Serializer
-  attributes :email
-
-  link :self do
-    @url_helpers.api_user_url(@object.id)
-  end
+  attribute :email
 end
