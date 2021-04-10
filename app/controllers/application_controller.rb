e074f258cdb6bfb555f5533eedd1de9d772633bf
rutger <36332815+rutger-t@users.noreply.github.com>
2021-04-10 10:38:36 +0900

    add todo message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  # TODO check for security issues with protect from forgery
   protect_from_forgery with: :null_session
 
   def render_jsonapi_response(resource)
