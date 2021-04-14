806ac1adf8e51da5fcbc80c8c7b2f7efdb974648
rutger <36332815+rutger-t@users.noreply.github.com>
2021-04-15 07:52:36 +0900

    add dummy login form

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router-dom'
 import FrontPage from './FrontPage/FrontPage'
 import Header from './Header/Header'
 import Footer from './Footer/Footer'
+import LoginForm from './User/LoginForm'
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <Header />
       <Switch>
         <Route exact path="/" component={FrontPage}/>
+        <Route exact path="/login" component={LoginForm}/>
       </Switch>
       <Footer />
     </div>
