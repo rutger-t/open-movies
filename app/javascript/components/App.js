c89c628820f8292cfb38ee7f13f71c17a777ac49
rutger <36332815+rutger-t@users.noreply.github.com>
2021-04-23 07:02:50 +0900

    use axios to retrieve user data from api

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -4,11 +4,31 @@ import FrontPage from './FrontPage/FrontPage'
 import Header from './Header/Header'
 import Footer from './Footer/Footer'
 import LoginForm from './User/LoginForm'
+import Axios from 'axios'
 
 const App = () => {
+  const [user, setUser] = React.useState();
+
+  React.useEffect(() => {
+    const config = {
+      headers: {
+        Authorization: 'Bearer ' + localStorage.getItem('token')
+      }
+    };
+
+    Axios.get('/api/v1/users/profile', config).then(
+      res => {
+        setUser(res.data['data']['attributes'])
+      },
+      err => {
+        console.log(err)
+      }
+    );
+  }, []);
+
   return (
     <div>
-      <Header />
+      <Header user={user} />
       <Switch>
         <Route exact path="/" component={FrontPage}/>
         <Route exact path="/login" component={LoginForm}/>
