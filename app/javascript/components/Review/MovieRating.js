fe3624c7c19ec3e685c56e9df0f227b65206b4bd
rutger <36332815+rutger-t@users.noreply.github.com>
2021-05-10 13:57:26 +0900

    create movie rating component

---
+++ app/javascript/components/Review/MovieRating.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { FaStar } from  'react-icons/fa';
+
+const MovieRating = (props) => {
+  const movie = props.movies[props.index]
+
+  return (
+    <div>
+      {[...Array(5)].map((star, i) => {
+        const ratingValue = i + 1;
+
+        return (
+          <FaStar
+            className="star"
+            color={ratingValue <= movie['vote_average'] ? "#ffc107" : "#e4e5e9"}
+            size={25}
+          />
+        );
+      })}
+      <p style={{margin: 0}}>{movie['vote_average']} stars based on {movie['vote_count']} ratings</p>
+    </div>
+  );
+}
+
+export default MovieRating
