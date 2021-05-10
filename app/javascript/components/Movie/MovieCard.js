1d69eb0cd456f1d2167277993e45d2b8bacf91d0
rutger <36332815+rutger-t@users.noreply.github.com>
2021-05-10 13:57:43 +0900

    add movie rating component to movie card

--- app/javascript/components/Movie/MovieCard.js
+++ app/javascript/components/Movie/MovieCard.js
@@ -4,11 +4,13 @@ import Button from '@material-ui/core/Button';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
+import CardHeader from '@material-ui/core/CardHeader';
 import CardMedia from '@material-ui/core/CardMedia';
 import Grid from '@material-ui/core/Grid';
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 import ReviewDialogue from './ReviewDialogue'
+import MovieRating from '../Review/MovieRating'
 
 const useStyles = makeStyles((theme) => ({
   cardGrid: {
@@ -43,10 +45,11 @@ const MovieCard = (props) => {
                 image={"https://image.tmdb.org/t/p/w500/" + movie.backdrop_path}
                 title="Image title"
               />
+              <CardHeader
+                title={movie.original_title}
+                subheader={<MovieRating movies={movies} index={index} />}
+              />
               <CardContent className={classes.cardContent}>
-                <Typography gutterBottom variant="h5" component="h2">
-                  {movie.original_title}
-                </Typography>
                 <Typography>
                   {movie.overview}
                 </Typography>
