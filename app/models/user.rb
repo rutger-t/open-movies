3cbe0f92b4a054e05c217d87e3c255b020aaa7c8
rutger <36332815+rutger-t@users.noreply.github.com>
2021-06-01 07:07:14 +0900

    fix dependent destroy

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :review
+  has_many :review, dependent: :destroy
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
